b1d1054bcd6bb9745e3cdb5de2ea3613129ba78c
Huaiwu Li <lhwzds@gmail.com>
2025-08-10 16:24:26 -0700

    feat: create workflow editor component

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,5 +1,14 @@
+import '@vue-flow/core/dist/style.css'
+import '@vue-flow/core/dist/theme-default.css'
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
 import { createApp } from 'vue'
-import './style.css'
 import App from './App.vue'
+import './style.css'
 
-createApp(App).mount('#app')
+
+createApp(App)
+
+const app = createApp(App)
+app.use(ElementPlus)
+app.mount('#app')
