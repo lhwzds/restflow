5675533f73d27830921e0e3dc4c4b025af6d9f48
Huaiwu Li <lhwzds@gmail.com>
2026-02-05 00:00:19 -0800

    feat: add release CI, npm package, and homebrew tap template

---
+++ homebrew-tap-template/Formula/restflow.rb
@@ -0,0 +1,34 @@
+class Restflow < Formula
+  desc "AI assistant that can execute workflows"
+  homepage "https://github.com/lhwzds/restflow"
+  version "0.1.0"
+  license "MIT"
+
+  on_macos do
+    if Hardware::CPU.arm?
+      url "https://github.com/lhwzds/restflow/releases/download/cli-v#{version}/restflow-aarch64-apple-darwin.tar.gz"
+      sha256 "PLACEHOLDER_SHA256_ARM"
+    else
+      url "https://github.com/lhwzds/restflow/releases/download/cli-v#{version}/restflow-x86_64-apple-darwin.tar.gz"
+      sha256 "PLACEHOLDER_SHA256_X86"
+    end
+  end
+
+  on_linux do
+    if Hardware::CPU.arm?
+      url "https://github.com/lhwzds/restflow/releases/download/cli-v#{version}/restflow-aarch64-unknown-linux-gnu.tar.gz"
+      sha256 "PLACEHOLDER_SHA256_LINUX_ARM"
+    else
+      url "https://github.com/lhwzds/restflow/releases/download/cli-v#{version}/restflow-x86_64-unknown-linux-gnu.tar.gz"
+      sha256 "PLACEHOLDER_SHA256_LINUX_X86"
+    end
+  end
+
+  def install
+    bin.install "restflow"
+  end
+
+  test do
+    assert_match "restflow #{version}", shell_output("#{bin}/restflow --version")
+  end
+end
---
+++ npm/README.md
@@ -0,0 +1,30 @@
+# RestFlow CLI npm Package
+
+This package provides the RestFlow CLI binary for multiple platforms.
+
+## Installation
+
+```bash
+npm install -g restflow-cli
+```
+
+## Usage
+
+After installation, the `restflow` command will be available:
+
+```bash
+restflow --help
+restflow daemon start
+restflow claude -p "Hello, world!"
+```
+
+## Supported Platforms
+
+- macOS (Intel & Apple Silicon)
+- Linux (x64 & ARM64)
+- Windows (x64)
+
+## Links
+
+- [GitHub Repository](https://github.com/lhwzds/restflow)
+- [Documentation](https://docs.restflow.ai)
