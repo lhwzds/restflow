9b5700ea0319811575dc1bc4e8876bc3f96a5ac6
Huaiwu Li <lhwzds@gmail.com>
2026-02-14 11:46:41 -0800

    feat(branding): apply 浮流 RestFlow naming updates (#583)

--- crates/restflow-cli/src/commands/info.rs
+++ crates/restflow-cli/src/commands/info.rs
@@ -1,6 +1,6 @@
 use anyhow::Result;
 
 pub fn run() -> Result<()> {
-    println!("RestFlow CLI {}", env!("CARGO_PKG_VERSION"));
+    println!("浮流 RestFlow CLI {}", env!("CARGO_PKG_VERSION"));
     Ok(())
 }
