e22a0423ca91e7158d9662042b995d31006852fb
Huaiwu Li <lhwzds@gmail.com>
2026-02-22 18:56:38 -0800

    remove(web): delete marketplace components and API module
    Marketplace backend API (api.restflow.dev) does not exist yet,
    making all marketplace UI non-functional. Remove dead frontend code
    until the backend service is available.

--- web/src/components/marketplace/index.ts
+++
@@ -1,7 +0,0 @@
-/**
- * Marketplace components for skill discovery and installation
- */
-
-export { default as SkillMarketplace } from './SkillMarketplace.vue'
-export { default as SkillCard } from './SkillCard.vue'
-export { default as SkillDetailModal } from './SkillDetailModal.vue'
