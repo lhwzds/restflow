6d0299c880ac174384cc3d1307ac2d4388626138
Huaiwu Li <lhwzds@gmail.com>
2026-02-23 21:16:44 -0800

    fix(composables): add 6 missing barrel exports
    Add useConfirm, useToast, and all workspace composables
    (useBackgroundAgentStream, useChatSession, useChatStream, useToolPanel)
    to the composables barrel file.

--- web/src/composables/index.ts
+++ web/src/composables/index.ts
@@ -3,4 +3,12 @@ export * from './secrets/useSecretOperations'
 export * from './secrets/useSecretsList'
 
 // Other composables
+export * from './useConfirm'
 export * from './useTheme'
+export * from './useToast'
+
+// Workspace composables
+export * from './workspace/useBackgroundAgentStream'
+export * from './workspace/useChatSession'
+export * from './workspace/useChatStream'
+export * from './workspace/useToolPanel'
